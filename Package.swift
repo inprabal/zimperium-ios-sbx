651f5f13521474375bf6971c11e7b5ccf85c3d69
inprabal <59173119+inprabal@users.noreply.github.com>
2024-09-06 10:54:13 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,24 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-    name: "ios-sbx",
+    name: "ZDefendSDK",
     platforms: [
-        .iOS(.v12) // Adjust to your minimum supported iOS version
+        .iOS(.v13)
     ],
     products: [
         .library(
-            name: "ios-sbx",
-            targets: ["ios-sbx"]
+            name: "ZDefendSDK",
+            targets: ["ZDefendSDK"]
         )
+    ],
+    dependencies: [
+        // Add other dependencies if needed
     ],
     targets: [
         .binaryTarget(
-            name: "ios-sbx",
-            url: "https://in.prabal%40gmail.com:cmVmdGtuOjAxOjE3NTcxMjQxMDY6VEs4cTU4Z3Jrc3hRUzI4Q1JyOW1aU2hYU2U5@zuday.jfrog.io/artifactory/zdefend-ios-swift/zimperium/5.4.46/ios-sbx/ios-sbx-5.4.46.zip",
+            name: "ZDefendSDK",
+            url: "https://vida-ro:1GiNW2oX$L6Xbci@zimperium.jfrog.io/artifactory/zdefend-releases/zimperium/sdk/zdefend-ios-sbx/5.4.46/zdefend-ios-sbx-5.4.46.zip",
             checksum: "22856d3bfafd2a6efccd4bd1a0ab5bc04ccaeed3c38089f6f72fce167a5242ee"
         )
     ]
